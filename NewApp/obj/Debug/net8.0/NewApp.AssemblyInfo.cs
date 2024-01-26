cdf25943e38ac198de2905398f1e88b7b97abbb3
sieunhangao0175 <levietcuong0175@gmail.com>
2024-01-26 13:27:03 +0700

    CreateProject

--- NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ddd87e1bbdc456ca08cc95a8b89a21a6b9d4ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+837a7b9bdbfd58cd3d2c896c4e31bbfe885bd682")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
