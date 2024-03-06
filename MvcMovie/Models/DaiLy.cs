3acead0d3d730ef0303b8d0918e45e8def3d7353
sieunhangao0175 <levietcuong0175@gmail.com>
2024-03-06 23:38:24 +0700

    asd

---
+++ MvcMovie/Models/DaiLy.cs
@@ -0,0 +1,13 @@
+namespace MvcMovie.Models;
+
+public class DaiLy 
+{
+    public string? MaDaiLy { get; set;}
+    public string? TenDaiLy{get; set;}
+    public string? DiaChi{get; set;}
+    public string? NguoiDaiDien{get; set;}
+    public string? Dienthoai{get; set;}
+    public string? MaHTTP{get; set;}
+
+
+}
--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+137583de1e5605361c3fc2ef6035c6cc54d6cf21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be1c702ceb174fb110b53e4ae907dc1398088c71")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
