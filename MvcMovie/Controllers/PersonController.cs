c753649feb7519e71fd368640ac5160615f3cc2e
sieunhangao0175 <levietcuong0175@gmail.com>
2024-05-16 10:21:18 +0700

    16

--- MvcMovie/Controllers/PersonController.cs
+++ MvcMovie/Controllers/PersonController.cs
@@ -1,22 +1,31 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
 using MvcMovie.Models;
-
+using NetMVC.Models.Process;
+using OfficeOpenXml;
 
 namespace MvcMovie.Controllers
 {
     public class PersonController : Controller
+{
+    private readonly ApplicationDbcontext _context;
+    public PersonController(ApplicationDbcontext context)
     {
-        private readonly ApplicationDbcontext _context;
-        public PersonController(ApplicationDbcontext context)
-        {
-            _context = context;
-        }
+        _context = context;
+    }
+    public ExcelProcess _excelProcess = new ExcelProcess();
     public async Task<IActionResult> Index()
     {
-        var model = await _context.Person.ToListAsync();
+        var model =  _context.Person.ToListAsync();
+        return View(model);
+
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Index(string SearchString)
+    {
+        var model = await _context.Person.Where(e => e.fullname.Contains(SearchString)).ToListAsync();
         return View(model);
     }
     public IActionResult Create()
@@ -25,7 +34,7 @@ namespace MvcMovie.Controllers
     }
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("PersonID,fullname,address")] Person person)
+    public async Task<IActionResult> Create([Bind("PersonId,FullName,Address")] Person person)
     {
         if (ModelState.IsValid)
         {
@@ -35,8 +44,9 @@ namespace MvcMovie.Controllers
 
         }
         return View(person);
+
     }
-    public async Task<IActionResult> Edit(string id)
+    public async Task<IActionResult>Edit(string id)
     {
         if (id == null || _context.Person == null)
         {
@@ -46,14 +56,17 @@ namespace MvcMovie.Controllers
         if (person == null)
         {
             return NotFound();
+
         }
         return View(person);
+
     }
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(string id, [Bind("personID,fullname,address")] Person person)
+    
+    public async Task<IActionResult> Edit(string id, [Bind("PersonId,FullName,Address")] Person person)
     {
-        if (id == person.PersonID)
+        if (id !=person.PersonID)
         {
             return NotFound();
 
@@ -64,57 +77,119 @@ namespace MvcMovie.Controllers
             {
                 _context.Update(person);
                 await _context.SaveChangesAsync();
+
             }
             catch (DbUpdateConcurrencyException)
             {
                 if (!PersonExists(person.PersonID))
                 {
                     return NotFound();
+
                 }
                 else
                 {
                     throw;
                 }
             }
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Index));           
         }
         return View(person);
     }
-    public async Task<IActionResult> Delete(string id)
+ public async Task<IActionResult> Delete(string id)
+ { 
+    if (id == null || _context.Person == null)
     {
-        if (id == null || _context.Person == null)
-        {
-            return NotFound();
-
-        }
-        var person = await _context.Person
-        .FirstOrDefaultAsync(m => m.PersonID == id);
-        if (person == null)
-        {
-            return NotFound();
-        }
-        return View(person);
+        return NotFound();
     }
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(string id)
+    var person = await _context.Person
+    .FirstOrDefaultAsync(m => m.PersonID == id);
+    if (person == null)
     {
-        if (_context.Person == null)
-        {
-            return Problem("Entiny set 'ApplicationDbcontext.Person' is null");
-        }
-        var person = await _context.Person.FindAsync(id);
-        if (person == null)
-        {
-            _context.Person.Remove(person);
-        }
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
+        return NotFound();
     }
-    private bool PersonExists(string id)
+    return View(person);
+ }
+ [HttpPost, ActionName("Delete")]
+ [ValidateAntiForgeryToken]
+  public async Task<IActionResult> DeleteConfirmed(string id)
+  {
+    if (_context.Person == null)
     {
-        return (_context.Person.Any(e => e.PersonID == id));
+        return Problem("Entity set 'ApplicationDbContext.Person' is null.");
     }
+    var person = await _context.Person.FindAsync(id);
+    if (person != null)
+    {
+        _context.Person.Remove(person);
+    }
+    await _context.SaveChangesAsync();
+    return RedirectToAction(nameof(Index));
+  }
+  public async Task<IActionResult> Upload()
+  {
+    return View();
+  }
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> Upload(IFormFile file)
+  {
+    if(file!=null)
+    {
+        string fileExtension = Path.GetExtension(file.FileName);
+        if(fileExtension != ".xls" && fileExtension != ".xlsx")
+        {
+            ModelState.AddModelError("", "Please choose excel file to upload !!!");
+        }
+        else
+        {
+                //rename fle when upload to sever
+                var FileName = DateTime.Now.ToShortTimeString() + fileExtension;
+                var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Upload/Excels", FileName);
+                var fileLocation = new FileInfo(filePath).ToString();
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    //save file to sever
+                    await file.CopyToAsync(stream);
+                    //read data from excel file fil datatable 
+                    var dt = _excelProcess.ExcelToDataTable(fileLocation);
+                    //using for loop to read data from dt
+                    for ( int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        var ps = new Person();
+                        ps.PersonID = dt.Rows[i][0].ToString();
+                        ps.fullname = dt.Rows[i][1].ToString();
+                        ps.address = dt.Rows[i][2].ToString();
+                        _context.Add(ps);
+                    }
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            } 
+        }
+    return View();
+  }
+  public ActionResult Download()
+  {
+    var fileName = "YourFileName" + ".xlsx";
+    using(ExcelPackage excelPackage = new ExcelPackage())
+    {
+        ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Sheet 1");
+        worksheet.Cells["A1"].Value = "PersonId";
+        worksheet.Cells["B1"].Value = "FullName";
+        worksheet.Cells["C1"].Value = "Address";
+         var personList = _context.Person.ToList();
+         worksheet.Cells["A2"].LoadFromCollection(personList);
+         var steam = new MemoryStream(excelPackage.GetAsByteArray());
+         return File(steam, "application/vn.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+    }
+  }
+  private bool PersonExists(string id)
+  {
+    return (_context.Person?.Any(e => e.PersonID == id)).GetValueOrDefault();
+  }
 }
 
+    public class ExcelProcess
+    {
+    }
 }
--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf19a2b35088cb6d99cfb7166ed15a5c281428d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb1534a3b4ce53761fb0cea392e1ebdd2f71606e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
