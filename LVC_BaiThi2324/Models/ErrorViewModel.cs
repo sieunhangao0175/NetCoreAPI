90b6a9c568f65399f1fd96af2f58f5a1bd9d3c24
sieunhangao0175 <151848856+sieunhangao0175@users.noreply.github.com>
2024-06-18 13:17:32 +0700

    Delete LVC_BaiThi2324 directory

--- LVC_BaiThi2324/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace LVC_BaiThi2324.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
